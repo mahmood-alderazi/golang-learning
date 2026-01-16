d6ad90f8792bc149b553284de42bd57d1bbdfe6b
Mahmood Alderazi. <88010753+mahmood-alderazi@users.noreply.github.com>
2026-01-16 12:56:32 +0300

    not sure it's 100% correct bro

---
+++ pointcheck/canjump.go
@@ -0,0 +1,31 @@
+package piscine
+
+func CanJump(input []uint) bool {
+
+	if len(input) == 1 {
+		return true
+	}
+
+	if len(input) == 0 {
+		return false
+	}
+
+	maxreach := 0
+
+	for i := 0; i < len(input); i++ {
+		if i > maxreach {
+			return false
+		}
+
+		jump := i + int(input[i])
+
+		if jump > maxreach {
+			maxreach = jump
+		}
+
+		if maxreach >= len(input)-1 {
+			return true
+		}
+	}
+	return false
+}
