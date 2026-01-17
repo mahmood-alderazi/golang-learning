51e5b1356bc6ccd195b1ad90abfdc61bcc5fb64b
Mahmood Alderazi. <88010753+mahmood-alderazi@users.noreply.github.com>
2026-01-17 14:29:14 +0300

    hello

---
+++ quest-4 /IterativeFactorial2.go
@@ -0,0 +1,19 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+
+	if nb <= 0 {
+		return 1
+	}
+
+	if nb == 0 {
+		return 1
+	}
+	result := 1
+
+	for i := 1; i <= nb; i++ {
+		result = result * i
+	}
+
+	return result
+}
