bdab3d1f496bc6d235e2b372a9be7d8233edd423
Mahmood Alderazi. <88010753+mahmood-alderazi@users.noreply.github.com>
2026-01-26 00:51:29 +0300

    Update fifthAndSkip2.go

--- pointcheck/fifthAndSkip2.go
+++ pointcheck/fifthAndSkip2.go
@@ -1,36 +1,36 @@
 package piscine
 
 func FifthAndSkip(str string) string {
-    if str == "" {
-        return "\n"
-    }
-    
-    // Remove spaces
-    clearStr := ""
-    for _, char := range str {
-        if char != ' ' {
-            clearStr += string(char)
-        }
-    }
-    
-    // Check length
-    if len(clearStr) < 5 {
-        return "Invalid Input\n"
-    }
-    
-    // Take 5, skip 1, repeat
-    result := ""
-    count := 0
-    
-    for _, char := range clearStr {
-        if count < 5 {
-            result += string(char)
-            count++
-        } else {
-            result += " "
-            count = 0
-        }
-    }
-    
-    return result + "\n"
+	if str == "" {
+		return "\n"
+	}
+
+	// Remove spaces
+	clearStr := ""
+	for _, char := range str {
+		if char != ' ' {
+			clearStr += string(char)
+		}
+	}
+
+	// Check length
+	if len(clearStr) < 5 {
+		return "Invalid Input\n"
+	}
+
+	// Take 5, skip 1, repeat
+	result := ""
+	count := 0
+
+	for _, char := range clearStr {
+		if count == 5 {
+			result += " "
+			count = 0
+		} else {
+			result += string(char)
+			count++
+		}
+	}
+
+	return result + "\n"
 }
